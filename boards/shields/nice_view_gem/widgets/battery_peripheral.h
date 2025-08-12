02268e1e74ccb07451bef314561ae48d0c3de103
Tony Hang <92767070+bigtoe2@users.noreply.github.com>
2025-08-12 15:34:38 -0500

    peri

---
+++ boards/shields/nice_view_gem/widgets/battery_peripheral.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <lvgl.h>
+#include "util.h"
+
+struct battery_status_state {
+    // uint8_t source; // 0: main, 1: peripheral
+    uint8_t level;
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+    bool usb_present;
+#endif
+};
+
+void draw_peripheral_status(lv_obj_t *canvas, const struct status_state *state);
