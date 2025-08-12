d203b168209bc1be851c4e25d7adb3c6ea712bc3
Tony Hang <92767070+bigtoe2@users.noreply.github.com>
2025-08-12 16:52:14 -0500

    changed declaration of peripheral_status_state

--- boards/shields/nice_view_gem/widgets/output.h
+++ boards/shields/nice_view_gem/widgets/output.h
@@ -11,11 +11,10 @@ struct output_status_state {
     bool active_profile_connected;
     bool active_profile_bonded;
 };
-#else
+#endif
 struct peripheral_status_state {
     bool connected;
 };
-#endif
 
 void draw_output_status(lv_obj_t *canvas, const struct status_state *state);
 void draw_peripheral_output_status(lv_obj_t *canvas, const struct status_state *state);
