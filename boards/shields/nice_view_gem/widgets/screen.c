baafdffffcc3c3a1bc0cd18689687192a5130d7a
Tony Hang <92767070+bigtoe2@users.noreply.github.com>
2025-08-15 15:07:11 -0500

    forgot canvas is rotated

--- boards/shields/nice_view_gem/widgets/screen.c
+++ boards/shields/nice_view_gem/widgets/screen.c
@@ -330,7 +330,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
 
     lv_obj_t *top = lv_canvas_create(widget->obj);
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
-    lv_canvas_set_buffer(top, widget->cbuf2, BUFFER_SIZE, 2*BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
+    lv_canvas_set_buffer(top, widget->cbuf2, 2*BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     // lv_obj_t *middle = lv_canvas_create(widget->obj);
     // lv_obj_align(middle, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_MIDDLE, 0);
